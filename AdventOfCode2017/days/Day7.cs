0e7aaef6eea0f0f31450af4448ab4fbca9f50440
luetojon <luetojon@students.zhaw>
2021-03-16 15:35:34 +0100

    Solved day 7

--- AdventOfCode2017/days/Day7.cs
+++ AdventOfCode2017/days/Day7.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using AdventOfCode2017.util;
@@ -9,13 +7,17 @@ namespace AdventOfCode2017.days
 {
     internal class Day7 : Day
     {
-        private string[] Programs { get; set; }
+        private string[] Programs { get; }
+        private Dictionary<string, int> NodesWeight { get; }
+        private Dictionary<string, string> NodesChildren { get; }
         public Day7(bool part1) : base(part1)
         {
             Input = File
                 .ReadAllText("C:\\Users\\Jonas\\RiderProjects\\AOC2017\\AdventOfCode2017\\resources\\input7.txt")
                 .Replace("\r", "");
             Programs = Input.Split("\n");
+            NodesWeight = new Dictionary<string, int>();
+            NodesChildren = new Dictionary<string, string>();
         }
 
         protected override string Solve1()
@@ -35,8 +37,6 @@ namespace AdventOfCode2017.days
 
         protected override string Solve2()
         {
-            var nodesWeight = new Dictionary<string, int>(); 
-            var nodesChildren = new Dictionary<string, string>(); 
             var rootName = Solve1();
 
             foreach (var line in Programs)
@@ -45,26 +45,76 @@ namespace AdventOfCode2017.days
                 var weight = int.Parse(line.Substring(line.IndexOf('(')+1).Split(")")[0]);
                 if (line.Contains("->"))
                 {
-                    nodesChildren.Add(name,line.Split("->")[1].Replace(" ",""));
+                    NodesChildren.Add(name,line.Split("->")[1].Replace(" ",""));
                 }
-                nodesWeight.Add(name, weight);
+                NodesWeight.Add(name, weight);
             }
 
-            var root = BuildTree(new Node(rootName, nodesWeight[rootName]), nodesChildren, nodesWeight);
-
-            return "";
+            var root = BuildTree(new Node(rootName, NodesWeight[rootName], (Node)null));
+            SumWeight(root);
+            
+            return GetWrongWeight(root,0).ToString();
         }
 
-        private Node BuildTree(Node node, Dictionary<string, string> nodesChildren, Dictionary<string, int> nodesWeight)
+        private Node BuildTree(Node node)
         {
-            if (!nodesChildren.ContainsKey(node.Name)) return node;
-            foreach (var childName in nodesChildren[node.Name].Split(","))
+            if (!NodesChildren.ContainsKey(node.Name)) return node;
+            foreach (var childName in NodesChildren[node.Name].Split(","))
             {
                 node.AddChild(
-                    BuildTree(new Node(childName, nodesWeight[childName]),nodesChildren,nodesWeight));
+                    BuildTree(new Node(childName, NodesWeight[childName],node)));
             }
 
             return node;
+        }
+
+        private int SumWeight(Node n)
+        {
+            foreach (var child in n.Children)
+            {
+                n.SumWeight += SumWeight(child);
+            }
+
+            return n.SumWeight;
+            /*
+            if (n.Children.Count != 0)
+            {
+                foreach (var child in n.Children)
+                {
+                    n.SumWeight += SumWeight(child);
+                }
+            }
+            return  n.Weight;
+            */
+        }
+
+        private int GetWrongWeight(Node node, int difference)
+        {
+            var weightCount = new Dictionary<int, int>();
+            foreach (var child in node.Children)
+            {
+                if (!weightCount.ContainsKey(child.SumWeight))
+                {
+                    weightCount.Add(child.SumWeight, 0);
+                }
+
+                var v = weightCount[child.SumWeight];
+                weightCount.Remove(child.SumWeight);
+                weightCount.Add(child.SumWeight,v+1);
+            }
+
+            var wrongWeight = weightCount.Keys.FirstOrDefault(key => weightCount[key] == 1);
+            
+            if (wrongWeight == 0)
+            {
+                return node.Weight + difference;
+            }
+            
+            difference = weightCount.Keys.FirstOrDefault(key => weightCount[key] > 1) - wrongWeight;
+
+            var t = node.Children.FirstOrDefault(child => child.SumWeight == wrongWeight);
+
+            return GetWrongWeight(t, difference);
         }
     }
 }
--- AdventOfCode2017/util/Node.cs
+++ AdventOfCode2017/util/Node.cs
@@ -8,19 +8,34 @@ namespace AdventOfCode2017.util
         public string Name { get; set; }
         public int Weight { get; set; }
         public List<Node> Children { get; set; }
+        public Node Parent { get; set; }
+        public int SumWeight { get; set; }
 
         public Node(string name, int weight,  List<Node> children)
         {
             Name = name;
             Weight = weight;
             Children = children;
+            Parent = null;
+            SumWeight = weight;
         }
         
-        public Node(string name, int weight)
+        public Node(string name, int weight, Node parent)
         {
             Name = name;
             Weight = weight;
-            Children = null;
+            Parent = parent;
+            Children = new List<Node>();
+            SumWeight = weight;
+        }
+
+        public Node(Node n)
+        {
+            Name = n.Name;
+            Weight = n.Weight;
+            Children = n.Children;
+            Parent = n.Parent;
+            SumWeight = n.SumWeight;
         }
 
         public void AddChild(Node child)
