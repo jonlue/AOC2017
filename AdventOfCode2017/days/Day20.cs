7fcc13ff35fc506a07e01b541238c227fab41884
luetojon <luetojon@students.zhaw>
2021-03-21 16:16:42 +0100

    Solve Day 20

---
+++ AdventOfCode2017/days/Day20.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using AdventOfCode2017.util;
+
+namespace AdventOfCode2017.days
+{
+    internal class Day20 : Day
+    {
+        private List<Particle> Particles { get; set; }
+        public Day20(string input, bool part1) : base(input, part1)
+        {
+            Input = Input.Replace("<", "")
+                .Replace(" ", "")
+                .Replace("p", "")
+                .Replace("v", "")
+                .Replace("a", "")
+                .Replace("=","")
+                .Replace(">,",">");
+            GenerateParticles();
+        }
+
+        protected override string Solve1()
+        {
+            var index = -1;
+            var i = 0;
+            var min = int.MaxValue;
+            
+            foreach (var particle in Particles)
+            {
+                particle.FakeSteps(100000);
+                var distance = particle.GetManhattanDistance();
+                if (distance < min)
+                {
+                    min = distance;
+                    index = i;
+                }
+
+                i++;
+            }
+
+            return index.ToString();
+        }
+
+        protected override string Solve2()
+        {
+            for (var i = 0; i < 10000; i++)
+            {
+                var positions = new Dictionary<Vector3, List<int>>();
+                var j = 0;
+                foreach (var particle in Particles)
+                {
+                    particle.MakeStep();
+                    if (!positions.ContainsKey(particle.Position))
+                    {
+                        positions.Add(particle.Position,new List<int>(){j});
+                    }
+                    else
+                    {
+                        positions[particle.Position].Add(j);
+                    }
+                    j++;
+                }
+
+                var count = 0;
+                foreach (var indices in positions.Values)
+                {
+                    if (indices.Count < 2) continue;
+                    foreach (var index in indices)
+                    {
+                        Particles.RemoveAt(index - count);
+                        count++;
+                    }
+                }
+            }
+
+            return Particles.Count.ToString();
+        }
+
+        private void GenerateParticles()
+        {
+            Particles = new List<Particle>();
+            foreach (var particle in Input.Split("\n"))
+            {
+                var p = particle.Split(">");
+                Particles.Add(new Particle(Array.ConvertAll(p[0].Split(","), int.Parse)
+                    ,Array.ConvertAll(p[1].Split(","), int.Parse)
+                    ,Array.ConvertAll(p[2].Split(","), int.Parse)));
+            }
+        }
+        
+    }
+}
---
+++ AdventOfCode2017/util/Particle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Numerics;
+
+namespace AdventOfCode2017.util
+{
+    public class Particle
+    {
+        public Vector3 Position { get; set; }
+        private Vector3 Velocity { get; set; }
+        private Vector3 Acceleration { get; }
+
+        public Particle(Vector3 position, Vector3 velocity, Vector3 acceleration)
+        {
+            Position = position;
+            Velocity = velocity;
+            Acceleration = acceleration;
+        }
+        
+        public Particle(int[] position, int[] velocity, int[] acceleration)
+        {
+            Position = new Vector3(position[0], position[1],position[2]);
+            Velocity = new Vector3(velocity[0],velocity[1],velocity[2]);
+            Acceleration = new Vector3(acceleration[0],acceleration[1],acceleration[2]);
+        }
+
+        public void MakeSteps(int steps)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                Velocity += Acceleration;
+                Position += Velocity;
+            }
+        }
+
+        public void MakeStep()
+        {
+            Velocity += Acceleration;
+            Position += Velocity;
+        }
+
+        public void FakeSteps(int times)
+        {
+            times = Math.Max(10000, times);
+            Position += Acceleration * times;
+        }
+
+        public int GetManhattanDistance(Vector3 point = default)
+        {
+            return (int) (Math.Abs(Position.X - point.X) + Math.Abs(Position.Y - point.Y) + Math.Abs(Position.Z - point.Z));
+        }
+    }
+}
